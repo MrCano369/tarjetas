d6b3f8cf2a9b02e651b3c016a1f47ff08f895200
MrCano369 <42789396+MrCano369@users.noreply.github.com>
2020-04-21 20:35:23 -0500

    Add files via upload

---
+++ consultarKanji.php
@@ -0,0 +1,10 @@
+<?php
+$con = mysqli_connect("localhost","id8994286_cano","123454321","id8994286_datos") or die ("Error1");
+$result = mysqli_query($con, "(select * from kanji)") or die ("Error2");
+
+$array = array();
+while($row = mysqli_fetch_array($result)){
+	array_push($array, $row);
+}
+echo json_encode($array);
+?>
